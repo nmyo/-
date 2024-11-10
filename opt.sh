c4e31da1ca9c43d6faad7efaf0947f933bfe086e
fangxi <52951423+nmyo@users.noreply.github.com>
2024-11-10 17:41:19 +0800

    Update opt.sh

--- opt.sh
+++ opt.sh
@@ -51,13 +51,13 @@ update_system() {
 install_dependencies() {
     case "$PACKAGE_MANAGER" in
         apt)
-            apt install -y curl vnstat sudo vim nload lsof dnsutils btop htop
+            apt install -y curl vnstat sudo vim nload lsof dnsutils btop
             ;;
         yum)
-            yum install -y curl epel-release vnstat sudo vim nload lsof bind-utils btop htop
+            yum install -y curl epel-release vnstat sudo vim nload lsof bind-utils btop
             ;;
         dnf)
-            dnf install -y curl epel-release vnstat sudo vim nload lsof bind-utils btop htop
+            dnf install -y curl epel-release vnstat sudo vim nload lsof bind-utils btop
             ;;
     esac
 }
@@ -140,16 +140,11 @@ optimize_kernel() {
     cat << EOL | tee /etc/sysctl.conf
 
 # ------ 网络调优: 基本 ------
-# TTL 配置, Linux 默认 64
 # net.ipv4.ip_default_ttl=64
-
-# 参阅 RFC 1323. 应当启用.
 net.ipv4.tcp_timestamps=1
 # ------ END 网络调优: 基本 ------
 
 # ------ 网络调优: 内核 Backlog 队列和缓存相关 ------
-# 有条件建议依据实测结果调整相关数值
-# 缓冲区相关配置均和内存相关
 net.core.wmem_default=16384
 net.core.rmem_default=262144
 net.core.rmem_max=536870912
@@ -163,18 +158,10 @@ net.core.netdev_max_backlog=10240
 net.ipv4.tcp_max_syn_backlog=10240
 net.core.somaxconn=8192
 net.ipv4.tcp_abort_on_overflow=1
-# 流控和拥塞控制相关调优
 net.core.default_qdisc=fq_pie
 net.ipv4.tcp_congestion_control=bbr
-# TCP 自动窗口
-# 要支持超过 64KB 的 TCP 窗口必须启用
 net.ipv4.tcp_window_scaling=1
-# 开启后, TCP 拥塞窗口会在一个 RTO 时间
-# 空闲之后重置为初始拥塞窗口 (CWND) 大小.
-# 大部分情况下, 尤其是大流量长连接, 设置为 0.
-# 对于网络情况时刻在相对剧烈变化的场景, 设置为 1.
 net.ipv4.tcp_slow_start_after_idle=0
-# nf_conntrack 调优
 net.nf_conntrack_max=1000000
 net.netfilter.nf_conntrack_max=1000000
 net.netfilter.nf_conntrack_tcp_timeout_fin_wait=30
@@ -182,60 +169,32 @@ net.netfilter.nf_conntrack_tcp_timeout_time_wait=30
 net.netfilter.nf_conntrack_tcp_timeout_close_wait=15
 net.netfilter.nf_conntrack_tcp_timeout_established=300
 net.ipv4.netfilter.ip_conntrack_tcp_timeout_established=7200
-# TIME-WAIT 状态调优
-# 只对客户端生效, 服务器连接上游时也认为是客户端
 net.ipv4.tcp_tw_reuse=1
-# 系统同时保持TIME_WAIT套接字的最大数量
-# 如果超过这个数字 TIME_WAIT 套接字将立刻被清除
 net.ipv4.tcp_max_tw_buckets=55000
 # ------ END 网络调优: 内核 Backlog 队列和缓存相关 ------
 
 # ------ 网络调优: 其他 ------
-# 启用选择应答
-# 对于广域网通信应当启用
 net.ipv4.tcp_sack=1
-# 启用转发应答
-# 对于广域网通信应当启用
 net.ipv4.tcp_fack=1
-# TCP SYN 连接超时重传次数
 net.ipv4.tcp_syn_retries=3
 net.ipv4.tcp_synack_retries=3
-# TCP SYN 连接超时时间, 设置为 5 约为 30s
 net.ipv4.tcp_retries2=5
-# 开启 SYN 洪水攻击保护
-# 勿听信所谓“安全优化教程”而无脑开启
 net.ipv4.tcp_syncookies=0
-# 开启反向路径过滤
-# Aliyun 负载均衡实例后端的 ECS 需要设置为 0
 net.ipv4.conf.default.rp_filter=2
 net.ipv4.conf.all.rp_filter=2
-# 减少处于 FIN-WAIT-2 连接状态的时间使系统可以处理更多的连接
 net.ipv4.tcp_fin_timeout=10
-# 默认情况下一个 TCP 连接关闭后, 把这个连接曾经有的参数保存到dst_entry中
-# 只要 dst_entry 没有失效,下次新建立相同连接的时候就可以使用保存的参数来初始化这个连接.通常情况下是关闭的
 net.ipv4.tcp_no_metrics_save=1
-# unix socket 最大队列
 net.unix.max_dgram_qlen=1024
-# 路由缓存刷新频率
 net.ipv4.route.gc_timeout=100
-# 启用 MTU 探测，在链路上存在 ICMP 黑洞时候有用（大多数情况是这样）
 net.ipv4.tcp_mtu_probing = 1
-# 开启并记录欺骗, 源路由和重定向包
 net.ipv4.conf.all.log_martians=1
 net.ipv4.conf.default.log_martians=1
-# 处理无源路由的包
 net.ipv4.conf.all.accept_source_route=0
 net.ipv4.conf.default.accept_source_route=0
-# TCP KeepAlive 调优
-# 最大闲置时间
 net.ipv4.tcp_keepalive_time=300
-# 最大失败次数, 超过此值后将通知应用层连接失效
 net.ipv4.tcp_keepalive_probes=2
-# 发送探测包的时间间隔
 net.ipv4.tcp_keepalive_intvl=2
-# 系统所能处理不属于任何进程的TCP sockets最大数量
 net.ipv4.tcp_max_orphans=262144
-# arp_table的缓存限制优化
 net.ipv4.neigh.default.gc_thresh1=128
 net.ipv4.neigh.default.gc_thresh2=512
 net.ipv4.neigh.default.gc_thresh3=4096
@@ -246,35 +205,20 @@ net.ipv4.conf.all.arp_announce=2
 # ------ END 网络调优: 其他 ------
 
 # ------ 内核调优 ------
-# 内核 Panic 后 1 秒自动重启
 kernel.panic=1
-# 允许更多的PIDs, 减少滚动翻转问题
 kernel.pid_max=32768
-# 内核所允许的最大共享内存段的大小（bytes）
 kernel.shmmax=4294967296
-# 在任何给定时刻, 系统上可以使用的共享内存的总量（pages）
 kernel.shmall=1073741824
-# 设定程序core时生成的文件名格式
 kernel.core_pattern=core_%e
-# 当发生oom时, 自动转换为panic
 vm.panic_on_oom=1
-# 表示强制Linux VM最低保留多少空闲内存（Kbytes）
-# vm.min_free_kbytes=1048576
-# 该值高于100, 则将导致内核倾向于回收directory和inode cache
 vm.vfs_cache_pressure=250
-# 表示系统进行交换行为的程度, 数值（0-100）越高, 越可能发生磁盘交换
-vm.swappiness=10
-# 仅用10%做为系统cache
+vm.swappiness=0
 vm.dirty_ratio=10
 vm.overcommit_memory=1
-# 增加系统文件描述符限制
-# Fix error: too many open files
 fs.file-max=2097152
 fs.inotify.max_user_instances=8192
 fs.inotify.max_user_instances=8192
-# 内核响应魔术键
 kernel.sysrq=1
-# 当某个节点可用内存不足时, 系统会倾向于从其他节点分配内存. 对 Mongo/Redis 类 cache 服务器友好
 vm.zone_reclaim_mode=0
 
 EOL
@@ -334,24 +278,20 @@ EOL
     echo -e "${GREEN}journald 配置已完成。${NC}"
 }
 
-# 配置 IPv4 优先
 configure_ipv4_priority() {
     # 检查是否存在有效的 IPv4 地址
     if ip -4 addr show | grep -E 'inet ' | grep -vE '127.0.0.1|::' > /dev/null; then
-        echo -e "${GREEN}检测到有效的 IPv4 地址，配置 IPv4 优先级。${NC}"
+        echo -e "${GREEN}检测到有效的 IPv4 地址，强制配置 IPv4 优先级。${NC}"
 
         # 备份原始配置文件
         cp /etc/gai.conf /etc/gai.conf.bak
 
-        # 设置 IPv4 优先
-        if grep -q '^precedence ::ffff:0:0/96  100' /etc/gai.conf; then
-            echo -e "${GREEN}IPv4 优先设置已经存在。${NC}"
-        else
-            sed -i 's/#precedence ::ffff:0:0\/96  100/precedence ::ffff:0:0\/96  100/' /etc/gai.conf
-            echo -e "${GREEN}IPv4 优先设置已应用。${NC}"
-        fi
+        # 强制写入 IPv4 优先配置
+        sed -i '/^#*precedence ::ffff:0:0\/96/d' /etc/gai.conf
+        echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
+        echo -e "${GREEN}IPv4 优先设置已强制写入。${NC}"
 
-        # 验证设置是否已成功应用
+        # 验证配置是否生效
         ip_output=$(curl -s ip.sb)
         if [[ $ip_output =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
             echo -e "${GREEN}IPv4 优先配置已成功应用: $ip_output${NC}"
